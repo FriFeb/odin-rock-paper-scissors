ae8aa0fe3e3ae88c1869553e11f1a1d9aaf2a46d
FriFeb <51440204+FriFeb@users.noreply.github.com>
2023-09-21 14:37:39 +0300

    refactor (playRound): separate showRoundResult function

--- javascript.js
+++ javascript.js
@@ -1,15 +1,17 @@
-/*
-getComputerChoice randomly returns either rock, paper or scissors.
-    /
-
-        generates random number from 0 to 2  
-
-    / ---> it returns the option corresponding to that number
-*/
-
 const ROCK = "Rock";
 const PAPER = "Paper";
 const SCISSORS = "Scissors";
+
+/*
+getComputerChoice function 
+  randomly returns either rock, paper or scissors.
+    /
+
+        generate random number from 0 to 2  
+        choose the option corresponding to that number
+
+    / ---> return the option
+*/
 
 function getComputerChoice() {
   const choice = Math.floor(Math.random() * 3);
@@ -25,9 +27,10 @@ function getComputerChoice() {
 }
 
 /*
-playRound plays a single round of the game, it takes 2 parameters - 
-the playerSelection and computerSelection - and then returns a string 
-that declares the winner of the round like so: "You Lose! Paper beats Rock"
+playRound function
+  plays a single round of the game, it takes 2 parameters - 
+  the playerSelection and computerSelection - and then returns a string 
+  that declares the winner of the round like so: "You Lose! Paper beats Rock"
     it takes a string with playerSelection 
     (should be case-sensitive "Rock", "rock", "ROCk")
     and the result of computerSelection function ---> /   
@@ -90,51 +93,127 @@ that declares the winner of the round like so: "You Lose! Paper beats Rock"
     like so: "You Lose! Paper beats Rock"
     or: "You Win! Rock beats Scissors"
     or: "Tie!"
+
+
+
+    //    //    //    NEW ALGORITHM     \\    \\    \\
+
+    playerSelection, computerSelection ---> /
+
+      same logic as above but returns
+        0 if user lose
+        1 if user win
+        2 if tie
+      calls showRoundResult function
+
+    / ---> result of the round
+
 */
 
 function playRound(playerSelection, computerSelection) {
+  let result;
+
   if (
     playerSelection === "ROCK" ||
     playerSelection === "Rock" ||
     playerSelection === "rock"
   )
     playerSelection = ROCK;
-
-  if (
+  else if (
     playerSelection === "PAPER" ||
     playerSelection === "Paper" ||
     playerSelection === "paper"
   )
     playerSelection = PAPER;
-
-  if (
+  else if (
     playerSelection === "SCISSORS" ||
     playerSelection === "Scissors" ||
     playerSelection === "scissors"
   )
     playerSelection = SCISSORS;
+  else return "Wrong variant!";
 
-  if (playerSelection === computerSelection) return "Tie!";
+  if (playerSelection === computerSelection) result = 2;
 
   if (playerSelection === ROCK) {
-    return computerSelection === PAPER
-      ? "You Lose! Paper beats Rock."
-      : "You Win! Rock beats Scissors.";
+    result = computerSelection === PAPER ? 0 : 1;
   }
 
   if (playerSelection === PAPER) {
-    return computerSelection === SCISSORS
-      ? "You Lose! Scissors beats Paper."
-      : "You Win! Paper beats Rock.";
+    result = computerSelection === SCISSORS ? 0 : 1;
   }
 
   if (playerSelection === SCISSORS) {
-    return computerSelection === ROCK
-      ? "You Lose! Rock beats Scissors."
-      : "You Win! Scissors beats Paper.";
+    result = computerSelection === ROCK ? 0 : 1;
+  }
+
+  showRoundResult(result, playerSelection, computerSelection);
+  return result;
+}
+
+/*
+showRoundResult function 
+  takes the result of playRound function
+  and prints a winner or loser
+
+    result of playRound function, playerSelection, computerSelection ---> /
+
+      IF result === 0
+        TRUE
+          print "You Lose! {computerSelection} beats {playerSelection}"
+
+      ELSE IF result === 1
+        TRUE 
+          print "You Win! {playerSelection} beats {computerSelection}"
+
+      ELSE
+        print "Tie!"
+
+    /
+*/
+
+function showRoundResult(result, playerSelection, computerSelection) {
+  if (result === 0) {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+  } else if (result === 1) {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  } else {
+    console.log("Tie!");
   }
 }
 
-const playerSelection = "Rock";
+/* 
+game function 
+  plays a 5 round game that keeps score 
+  and reports a winner or loser at the end
+
+    it takes computerSelection and playerSelection ---> /
+
+      play 5 rounds
+        call playRound function 5 times
+      keep score 
+        - rewrite playRound function so it returns true 
+        if user win and false if user lose
+        - create showRoundResult function that prints 
+        winner or loser of the round
+        - create userScore and computerScore with value 0
+        - add +1 to userScore if user wins
+        - add +1 to computerScore if computer wins
+      report a winner or loser 
+        - IF userScore > computerScore
+            TRUE
+              print "You win the game!"
+            FALSE
+              print "You lose the game!"
+
+    / ---> it shows the winner of the game with console.log()
+*/
+
+function game() {}
+
+const playerSelection = prompt(
+  "Enter your choice (rock, paper, scissors)",
+  "Rock"
+);
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+playRound(playerSelection, computerSelection);
